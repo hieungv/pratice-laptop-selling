b737443dca566fd83f093433a20b396585a8cb77
Đào Hùng <dao.van.hung@framgia.com>
2019-02-20 21:09:18 +0700

    Create pull_request_template.md

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,31 @@
+class StaticPagesController < ApplicationController
+  def compair; end
+
+  def components; end
+
+  def contact; end
+
+  def faq; end
+
+  def forgetpass; end
+
+  def index; end
+
+  def legal_notice; end
+
+  def login; end
+
+  def normal; end
+
+  def product_details; end
+
+  def products; end
+
+  def product_summary; end
+
+  def register; end
+
+  def special_offer; end
+
+  def tac; end
+end
