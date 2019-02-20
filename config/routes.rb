5bf61ee910e87f5e3804179a2dac591caab7f63e
Đào Hùng <dao.van.hung@framgia.com>
2019-02-20 21:09:18 +0700

    Static Pages Header

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  get 'static_pages/compair'
-  get 'static_pages/components'
-  get 'static_pages/contact'
-  get 'static_pages/faq'
-  get 'static_pages/forgetpass'
-  get 'static_pages/index'
-  get 'static_pages/legal_notice'
-  get 'static_pages/login'
-  get 'static_pages/normal'
-  get 'static_pages/product_details'
-  get 'static_pages/products'
-  get 'static_pages/product_summary'
-  get 'static_pages/register'
-  get 'static_pages/special_offer'
-  get 'static_pages/tac'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#index"
+  get "static_pages/compair"
+  get "static_pages/components"
+  get "static_pages/contact"
+  get "static_pages/faq"
+  get "static_pages/forgetpass"
+  get "static_pages/legal_notice"
+  get "static_pages/login"
+  get "static_pages/normal"
+  get "static_pages/product_details"
+  get "static_pages/products"
+  get "static_pages/product_summary"
+  get "static_pages/register"
+  get "static_pages/special_offer"
+  get "static_pages/tac"
 end
