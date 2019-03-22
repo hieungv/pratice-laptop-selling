5e2bad1a5862ebadeb807ffd6a8f21fd78bc7f8f
memsenpai <memsenpai3@gmail.com>
2019-03-22 17:43:50 +0700

    phungthiha

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,15 @@
 
 User.create!(name: "Ha", email: "ha@gmail.com",
   password: "123123", password_confirmation: "123123", admin: true)
-Image.create!(product_id: "1", image: "products/6.jpg")
-Product.create!(name: "Laptop xxx",
-             import_price: "200",
-             export_price: "300",
-             status: "1",
-             image: "1",
-             sale: "40")
-
 99.times do |n|
   name  = Faker::Name.name
-  Product.create!(name: name,
-             import_price: "200",
-             export_price: "300",
+  import_price = rand(500..2000)
+  image_id = rand(1..8)
+  product = Product.create!(name: name,
+             import_price: import_price,
+             export_price: import_price + 100,
              status: "1",
-             image: "1",
+             image: rand(1..8),
              sale: "40")
+  product.images.create image: "laptop/#{image_id}.jpg"
 end
